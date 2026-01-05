a35014e8e2e46bbde8ba9223557c3d4a4b718084
Dima Shapranov <shapranov.work@gmail.com>
2026-01-05 14:33:38 +0300

    del lock file

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -7,26 +7,16 @@ from foxypack_youtube_pytubefix import (
     FoxyYouTubeAnalysis,
 )
 
-
-@pytest.fixture(scope="session")
-def test_proxy():
-    test_proxy = "http://admin:t!2537fMP3_LM759Rg2B@144.124.241.107:49391"
-    return test_proxy
-
-
-def test_proxy_entity(test_proxy):
+def test_proxy_entity():
     """Test case for getting statistics for a video"""
-
-    proxy = YoutubeProxy(proxy_str=test_proxy)
     controller = EntitiesController()
-    controller.add_entity(proxy)
-    youtube_stat_ = YouTubeVideo(entities_controller=controller)
-    youtube_stat_two_ = YouTubeVideo(entities_controller=controller)
+    youtube_stat_one = YouTubeVideo()
+    youtube_stat_two = YouTubeVideo()
     youtube_analysis = FoxyYouTubeAnalysis().get_analysis(
         "https://www.youtube.com/watch?v=SNfrBPoHCTY"
     )
-    stat_one = youtube_stat_.get_statistics(youtube_analysis)
-    stat_two = youtube_stat_two_.get_statistics(youtube_analysis)
+    stat_one = youtube_stat_one.get_statistics(youtube_analysis)
+    stat_two = youtube_stat_two.get_statistics(youtube_analysis)
     assert stat_one.answer_id != stat_two.answer_id
     assert stat_one.system_id == stat_two.system_id
     assert stat_one.title == stat_two.title
@@ -40,11 +30,9 @@ def test_proxy_entity(test_proxy):
     assert stat_one.duration == stat_two.duration
 
 
-def test_proxy_entity_two_link_format(test_proxy):
+def test_proxy_entity_two_link_format():
     """Test case for getting statistics for a video"""
-    proxy = YoutubeProxy(proxy_str=test_proxy)
     controller = EntitiesController()
-    controller.add_entity(proxy)
     youtube_stat = YouTubeVideo(entities_controller=controller)
     youtube_stat_two = YouTubeVideo(entities_controller=controller)
     youtube_analysis = FoxyYouTubeAnalysis().get_analysis(
@@ -65,11 +53,9 @@ def test_proxy_entity_two_link_format(test_proxy):
     assert stat_one.duration == stat_two.duration
 
 
-def test_get_statistics_video_three_link_format(test_proxy):
+def test_get_statistics_video_three_link_format():
     """Test case for getting statistics for a video"""
-    proxy = YoutubeProxy(proxy_str=test_proxy)
     controller = EntitiesController()
-    controller.add_entity(proxy)
     youtube_stat = YouTubeVideo(entities_controller=controller)
     youtube_stat_two = YouTubeVideo(entities_controller=controller)
     youtube_analysis = FoxyYouTubeAnalysis().get_analysis(
@@ -90,11 +76,9 @@ def test_get_statistics_video_three_link_format(test_proxy):
     assert stat_one.duration == stat_two.duration
 
 
-def test_get_statistics_channel_foxy_stat(test_proxy):
+def test_get_statistics_channel_foxy_stat():
     """Test case for getting statistics for a channel"""
-    proxy = YoutubeProxy(proxy_str=test_proxy)
     controller = EntitiesController()
-    controller.add_entity(proxy)
     youtube_stat = YouTubeChannel(entities_controller=controller)
     youtube_stat_two = YouTubeChannel(entities_controller=controller)
     youtube_analysis = FoxyYouTubeAnalysis().get_analysis(
@@ -116,11 +100,9 @@ def test_get_statistics_channel_foxy_stat(test_proxy):
     assert stat_one.analysis_status == stat_two.analysis_status
 
 
-def test_get_statistics_channel_foxy_stat_two(test_proxy):
+def test_get_statistics_channel_foxy_stat_two():
     """Test case for getting statistics for a channel"""
-    proxy = YoutubeProxy(proxy_str=test_proxy)
     controller = EntitiesController()
-    controller.add_entity(proxy)
     youtube_stat = YouTubeChannel(entities_controller=controller)
     youtube_stat_two = YouTubeChannel(entities_controller=controller)
     youtube_analysis = FoxyYouTubeAnalysis().get_analysis(
@@ -140,3 +122,136 @@ def test_get_statistics_channel_foxy_stat_two(test_proxy):
     assert stat_one.number_videos == stat_two.number_videos
     assert stat_one.external_link == stat_two.external_link
     assert stat_one.analysis_status == stat_two.analysis_status
+
+# @pytest.fixture(scope="session")
+# def test_proxy():
+#     test_proxy = "http://admin:74RSV_k%kcF8S6RRe6mh@144.124.241.107:24731"
+#     return test_proxy
+
+
+# def test_proxy_entity(test_proxy):
+#     """Test case for getting statistics for a video"""
+#
+#     proxy = YoutubeProxy(proxy_str=test_proxy)
+#     controller = EntitiesController()
+#     controller.add_entity(proxy)
+#     youtube_stat_ = YouTubeVideo(entities_controller=controller)
+#     youtube_stat_two_ = YouTubeVideo(entities_controller=controller)
+#     youtube_analysis = FoxyYouTubeAnalysis().get_analysis(
+#         "https://www.youtube.com/watch?v=SNfrBPoHCTY"
+#     )
+#     stat_one = youtube_stat_.get_statistics(youtube_analysis)
+#     stat_two = youtube_stat_two_.get_statistics(youtube_analysis)
+#     assert stat_one.answer_id != stat_two.answer_id
+#     assert stat_one.system_id == stat_two.system_id
+#     assert stat_one.title == stat_two.title
+#     assert stat_one.views == stat_two.views
+#     assert stat_one.publish_date == stat_two.publish_date
+#     assert stat_one.analysis_status == stat_two.analysis_status
+#     assert stat_one.channel_id == stat_two.channel_id
+#     assert stat_one.likes == stat_two.likes
+#     assert stat_one.link == stat_two.link
+#     assert stat_one.channel_url == stat_two.channel_url
+#     assert stat_one.duration == stat_two.duration
+#
+#
+# def test_proxy_entity_two_link_format(test_proxy):
+#     """Test case for getting statistics for a video"""
+#     proxy = YoutubeProxy(proxy_str=test_proxy)
+#     controller = EntitiesController()
+#     controller.add_entity(proxy)
+#     youtube_stat = YouTubeVideo(entities_controller=controller)
+#     youtube_stat_two = YouTubeVideo(entities_controller=controller)
+#     youtube_analysis = FoxyYouTubeAnalysis().get_analysis(
+#         "https://youtu.be/PZHESOq-Gkw?t=376"
+#     )
+#     stat_one = youtube_stat.get_statistics(youtube_analysis)
+#     stat_two = youtube_stat_two.get_statistics(youtube_analysis)
+#     assert stat_one.answer_id != stat_two.answer_id
+#     assert stat_one.system_id == stat_two.system_id
+#     assert stat_one.title == stat_two.title
+#     assert stat_one.views == stat_two.views
+#     assert stat_one.publish_date == stat_two.publish_date
+#     assert stat_one.analysis_status == stat_two.analysis_status
+#     assert stat_one.channel_id == stat_two.channel_id
+#     assert stat_one.likes == stat_two.likes
+#     assert stat_one.link == stat_two.link
+#     assert stat_one.channel_url == stat_two.channel_url
+#     assert stat_one.duration == stat_two.duration
+#
+#
+# def test_get_statistics_video_three_link_format(test_proxy):
+#     """Test case for getting statistics for a video"""
+#     proxy = YoutubeProxy(proxy_str=test_proxy)
+#     controller = EntitiesController()
+#     controller.add_entity(proxy)
+#     youtube_stat = YouTubeVideo(entities_controller=controller)
+#     youtube_stat_two = YouTubeVideo(entities_controller=controller)
+#     youtube_analysis = FoxyYouTubeAnalysis().get_analysis(
+#         "https://youtu.be/PZHESOq-Gkw"
+#     )
+#     stat_one = youtube_stat.get_statistics(youtube_analysis)
+#     stat_two = youtube_stat_two.get_statistics(youtube_analysis)
+#     assert stat_one.answer_id != stat_two.answer_id
+#     assert stat_one.system_id == stat_two.system_id
+#     assert stat_one.title == stat_two.title
+#     assert stat_one.views == stat_two.views
+#     assert stat_one.publish_date == stat_two.publish_date
+#     assert stat_one.analysis_status == stat_two.analysis_status
+#     assert stat_one.channel_id == stat_two.channel_id
+#     assert stat_one.likes == stat_two.likes
+#     assert stat_one.link == stat_two.link
+#     assert stat_one.channel_url == stat_two.channel_url
+#     assert stat_one.duration == stat_two.duration
+#
+#
+# def test_get_statistics_channel_foxy_stat(test_proxy):
+#     """Test case for getting statistics for a channel"""
+#     proxy = YoutubeProxy(proxy_str=test_proxy)
+#     controller = EntitiesController()
+#     controller.add_entity(proxy)
+#     youtube_stat = YouTubeChannel(entities_controller=controller)
+#     youtube_stat_two = YouTubeChannel(entities_controller=controller)
+#     youtube_analysis = FoxyYouTubeAnalysis().get_analysis(
+#         "https://www.youtube.com/@KINOKOS"
+#     )
+#     stat_one = youtube_stat.get_statistics(youtube_analysis)
+#     stat_two = youtube_stat_two.get_statistics(youtube_analysis)
+#     assert stat_one.answer_id != stat_two.answer_id
+#     assert stat_one.title == stat_two.title
+#     assert stat_one.link == stat_two.link
+#     assert stat_one.description == stat_two.description
+#     assert stat_one.country == stat_two.country
+#     assert stat_one.system_id == stat_two.system_id
+#     assert stat_one.view_count == stat_two.view_count
+#     assert stat_one.subscribers == stat_two.subscribers
+#     assert stat_one.creation_date == stat_two.creation_date
+#     assert stat_one.number_videos == stat_two.number_videos
+#     assert stat_one.external_link == stat_two.external_link
+#     assert stat_one.analysis_status == stat_two.analysis_status
+#
+#
+# def test_get_statistics_channel_foxy_stat_two(test_proxy):
+#     """Test case for getting statistics for a channel"""
+#     proxy = YoutubeProxy(proxy_str=test_proxy)
+#     controller = EntitiesController()
+#     controller.add_entity(proxy)
+#     youtube_stat = YouTubeChannel(entities_controller=controller)
+#     youtube_stat_two = YouTubeChannel(entities_controller=controller)
+#     youtube_analysis = FoxyYouTubeAnalysis().get_analysis(
+#         "https://www.youtube.com/channel/UCj2QqbeCUZ82JMk492iGUQg"
+#     )
+#     stat_one = youtube_stat.get_statistics(youtube_analysis)
+#     stat_two = youtube_stat_two.get_statistics(youtube_analysis)
+#     assert stat_one.answer_id != stat_two.answer_id
+#     assert stat_one.title == stat_two.title
+#     assert stat_one.link == stat_two.link
+#     assert stat_one.description == stat_two.description
+#     assert stat_one.country == stat_two.country
+#     assert stat_one.system_id == stat_two.system_id
+#     assert stat_one.view_count == stat_two.view_count
+#     assert stat_one.subscribers == stat_two.subscribers
+#     assert stat_one.creation_date == stat_two.creation_date
+#     assert stat_one.number_videos == stat_two.number_videos
+#     assert stat_one.external_link == stat_two.external_link
+#     assert stat_one.analysis_status == stat_two.analysis_status
